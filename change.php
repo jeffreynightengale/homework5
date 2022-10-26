15ed7e8b04a11abac3240868758fcdd1e56ef97c
Jeffrey Nightengale <78056207+jeffreynightengale@users.noreply.github.com>
2022-10-26 13:54:58 -0500

    Create change.php

---
+++ change.php
@@ -0,0 +1,13 @@
+<?php require_once("header.php"); ?>
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>What Can JavaScript Do?</h2>
+
+<p id="demo">JavaScript can change HTML content.</p>
+
+<button type="button" onclick='document.getElementById("demo").innerHTML = "Hello User! Thanks for changing the sentence!"'>Click Me!</button>
+
+</body>
+</html>
